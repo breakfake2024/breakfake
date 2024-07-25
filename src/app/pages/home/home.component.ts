a0e42aee6cf3174c1e2d560367d996e34523bb85
Yurii Ohlii <jukbq77@gmail.com>
2024-07-25 15:03:31 +0300

    Initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  ngOnInit(){
+    console.log('Hello');
+    
+  };
+
+  open_country(country: any) {
+    console.log(country);
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+
+
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAiqDIFGjQMx03xnEs9W4FbqoXrugZw7xo',
+    authDomain: 'breakfake-54baa.firebaseapp.com',
+    projectId: 'breakfake-54baa',
+    storageBucket: 'breakfake-54baa.appspot.com',
+    messagingSenderId: '374592407666',
+    appId: '1:374592407666:web:6001353bef3187bbb21aeb',
+  },
+};

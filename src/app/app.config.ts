394bbcb8616262a2a3e5c00f89864a8dfb888076
Yurii Ohlii <jukbq77@gmail.com>
2024-07-25 15:16:20 +0300

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"breakfake-54baa","appId":"1:374592407666:web:6001353bef3187bbb21aeb","storageBucket":"breakfake-54baa.appspot.com","apiKey":"AIzaSyAiqDIFGjQMx03xnEs9W4FbqoXrugZw7xo","authDomain":"breakfake-54baa.firebaseapp.com","messagingSenderId":"374592407666"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
 };
